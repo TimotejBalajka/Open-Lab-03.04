4d302cf9c7c65d9fba6f996a5bc2ab6c9d4b26bf
TimotejBalajka <113421940+TimotejBalajka@users.noreply.github.com>
2022-09-24 23:02:47 +0200

    Update Numbers.cs

--- Open-Lab-03.04/Numbers.cs
+++ Open-Lab-03.04/Numbers.cs
@@ -6,7 +6,10 @@ namespace Open_Lab_03._04
     {
         public string EvenOrOdd(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 ==0)
+                return "odd";
+            else
+                return "even";
         }
     }
 }
